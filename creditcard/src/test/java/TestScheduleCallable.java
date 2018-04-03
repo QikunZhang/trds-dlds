ae36ba9c12e79d6ba7312e4586e066f644eea715
QikunZhang <33598337+QikunZhang@users.noreply.github.com>
2018-04-03 18:38:45 +0800

    测试一次

--- creditcard/src/test/java/TestScheduleCallable.java
+++ creditcard/src/test/java/TestScheduleCallable.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+
 import com.paic.tsds.tool.template.dto.SwapDto;
 import com.paic.tsds.tool.template.service.schedule.BaseCallable;
 import com.paic.tsds.tool.template.service.schedule.EndLogic;
@@ -9,12 +10,13 @@ public class TestScheduleCallable {
 
     public static BaseCallable sleepTask() {
 	return new BaseCallable() {
-	    private long sleepTime = 3000l;
+	    private long sleepTime = 30l;
 
 	    public SwapDto execute(SwapDto inSwapDto) {
 		Map param = (Map) inSwapDto.getData();
 		try {
 		    Thread.sleep(sleepTime);
+		    System.out.println("lalalala");
 		    ((Map) param.get("tmpMap")).put(sleepTime, "true");
 		} catch (InterruptedException e) {
 		    ((Map) param.get("tmpMap")).put(sleepTime, "false");
@@ -28,8 +30,8 @@ public class TestScheduleCallable {
 
     public static ScheduleMap releaseSchedule() {
 	ScheduleMap sm = new ScheduleMap();
-	sm.putSchedule(1, new Long(5000), EndLogic.ALLSUCCESS);
-	sm.putSchedule(2, new Long(2000), EndLogic.ALLFAIL);
+	sm.putSchedule(1, new Long(500), EndLogic.ALLSUCCESS);
+	sm.putSchedule(2, new Long(200), EndLogic.ALLFAIL);
 
 	sm.putCallable(1, sleepTask());
 	sm.putCallable(1, sleepTask());
